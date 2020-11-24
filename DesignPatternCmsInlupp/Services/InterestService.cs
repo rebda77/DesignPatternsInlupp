930c957bd7187aff491313b33270a2db87aaf6ae
rebec <rebec@LAPTOP-EU9FE752>
2020-11-24 16:23:30 +0100

    Add Retry pattern

--- DesignPatternCmsInlupp/Services/InterestService.cs
+++ DesignPatternCmsInlupp/Services/InterestService.cs
@@ -9,18 +9,37 @@ namespace DesignPatternCmsInlupp.Services
     {
         public static decimal GetRiksbankensBaseRate()
         {
-            //Fake slow call
-            System.Threading.Thread.Sleep(5000);
-            using (var c = new SweaWebService.SweaWebServicePortTypeClient())
+            int retries = 5;
+            int sleepWhenFailBeforeRetry = 3000;
+            while(retries>0)
             {
-                //var groups = c.getInterestAndExchangeGroupNames(SweaWebService.LanguageType.sv).ToList();
+                try
+                {
+                    //Fake slow call
+                    //System.Threading.Thread.Sleep(5000);
+                    using (var c = new SweaWebService.SweaWebServicePortTypeClient())
+                    {
+                        //var groups = c.getInterestAndExchangeGroupNames(SweaWebService.LanguageType.sv).ToList();
 
-                //var n = c.getInterestAndExchangeNames(5, SweaWebService.LanguageType.sv).ToList();
+                        //var n = c.getInterestAndExchangeNames(5, SweaWebService.LanguageType.sv).ToList();
 
-                var r = c.getLatestInterestAndExchangeRates(SweaWebService.LanguageType.sv, new[] { "SEDP3MSTIBORDELAYC" });
-                
-                return Convert.ToDecimal(r.groups[0].series[0].resultrows[0].value);
+                        var r = c.getLatestInterestAndExchangeRates(SweaWebService.LanguageType.sv, new[] { "SEDP3MSTIBORDELAYC" });
+
+                        return Convert.ToDecimal(r.groups[0].series[0].resultrows[0].value);
+                    }
+
+                }
+                catch
+                {
+                    System.Threading.Thread.Sleep(sleepWhenFailBeforeRetry);
+                    retries 5; // eller --;
+                    // test 
+                }
+
             }
+            return "ERROR";
+
+            
         }
     }
 }
